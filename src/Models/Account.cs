eabe5624ef45331add6f3b1ce8340ace72ebc52f
dancita <daniella.paal@gmail.com>
2023-03-18 10:58:10 +0000

    Refactoring, renaming test project, adding XML comments to properties

--- src/Models/Account.cs
+++ src/Models/Account.cs
@@ -4,9 +4,20 @@ namespace AccountManagementAPI.Models
 {
     public class Account
     {
+        /// <summary>
+        /// Id of the account
+        /// </summary>
         [Key]
         public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the account holder
+        /// </summary>
         public string? AccountHolderName { get; set; }
+
+        /// <summary>
+        /// Status of the account
+        /// </summary>
         public bool IsActive { get; set; }
     }
 }
--- src/Models/PhoneNumber.cs
+++ src/Models/PhoneNumber.cs
@@ -4,9 +4,20 @@ namespace AccountManagementAPI.Models
 {
     public class PhoneNumber
     {
+        /// <summary>
+        /// Id of the phone number
+        /// </summary>
         [Key]
         public int Id { get; set; }
+
+        /// <summary>
+        /// The phone number itself
+        /// </summary>
         public string? Number { get; set; }
+
+        /// <summary>
+        /// The id of the account assigned to the phone number
+        /// </summary>
         public int? AccountId { get; set; }
     }
 }
